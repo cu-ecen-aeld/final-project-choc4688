a0ba53269fa604e7ff6cf943f38b8b855fc85392
chaseo <chaseo@Ubuntu.myguest.virtualbox.org>
2025-11-30 23:49:20 -0700

    Buildroot recipe changes

--- base_external/package/final-application/final-application.mk
+++ base_external/package/final-application/final-application.mk
@@ -1,11 +1,10 @@
 
-FINAL_APPLICATION_VERSION = 7fa97d676a3b34b7162a40bd940d386aa190ffbd
+FINAL_APPLICATION_VERSION = b5b2c090270d71cfc008f9d73a0b2fdb7dad55ad
 FINAL_APPLICATION_SITE = git@github.com:choc4688/final-project-source-choc4688.git
 FINAL_APPLICATION_SITE_METHOD = git
 FINAL_APPLICATION_GIT_SUBMODULES = YES 
 
 FINAL_APPLICATION_DEPENDENCIES = lwip
-
 
 define FINAL_APPLICATION_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/lwip_app
@@ -13,9 +12,11 @@ endef
 
 # @D = Build Directory
 define FINAL_APPLICATION_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/lwip_app/lwip_app $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/lwip_app/testlwip-start-stop.sh $(TARGET_DIR)/etc/init.d/S99testlwip
+	$(INSTALL) -m 0755 $(@D)/lwip_app/udpecho $(TARGET_DIR)/usr/bin
 endef
+
+# 	$(INSTALL) -m 0755 $(@D)/lwip_app/lwip_app $(TARGET_DIR)/usr/bin
+# 	$(INSTALL) -m 0755 $(@D)/lwip_app/testlwip-start-stop.sh $(TARGET_DIR)/etc/init.d/S99testlwip
 
 
 $(eval $(generic-package))
--- base_external/package/lwip/lwip.mk
+++ base_external/package/lwip/lwip.mk
@@ -11,6 +11,19 @@ LWIP_MODULE_SUBDIRS += api
 LWIP_MODULE_SUBDIRS += core
 LWIP_MODULE_SUBDIRS += include
 
+#Needed because my lwip_app Makefile requires my lwip to be installed already
+#because of dependencies, and apparently Buildroot only checks the staging directory only
+#rather than target directory when doing cross-compilation dependency resolution.
+LWIP_INSTALL_STAGING = YES
+
+#Below staging section partially from ChatGPT
+define LWIP_INSTALL_STAGING_CMDS
+	$(INSTALL) -d $(STAGING_DIR)/lwip
+	$(shell mkdir -p $(STAGING_DIR)/usr/lib/lwip/src)
+	cp -a -R $(@D)/* $(STAGING_DIR)/usr/lib/lwip/
+endef
+# 	cp -a $(@D)/src/Filelists.mk $(STAGING_DIR)/usr/lib/lwip/src/
+
 
 define LWIP_INSTALL_TARGET_CMDS
 	$(INSTALL) -d $(TARGET_DIR)/usr/lib/lwip
